1df3f8044a7b02c70a5577e0a78d9fce06c34c7d
Resalat islam <resalatislam5@gmail.com>
2024-10-23 05:38:40 +0600

    created task add section ui

---
+++ src/app/components/ui/taskFrom/index.jsx
@@ -0,0 +1,24 @@
+import styles from "./taskFrom.module.css";
+function TaskFrom({ title }) {
+  // TODO: implement form functionality
+  return (
+    <section className={styles.main}>
+      {/* Main header */}
+      <h1>{title}</h1>
+      {/* input from */}
+      <form className={styles.form}>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="day-month-year" />
+        <select name="" id="">
+          <option value="">Low</option>
+          <option value="">Medium</option>
+          <option value="">High</option>
+        </select>
+        <input type="text" placeholder="Work, Personal, Shopping" />
+        <input type="submit" value="Submit" />
+      </form>
+    </section>
+  );
+}
+
+export default TaskFrom;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
+import TaskFrom from "./components/ui/taskFrom";
+import styles from "./home.module.css";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className={styles.form_section}>
+      <TaskFrom title={"Task Add"} />
+    </div>
+  );
 };
 
 export default Home;
